4c7151767e08b40841f21a07ec539e031e6f05d1
yelisha <62861833+yelisha@users.noreply.github.com>
2021-10-10 17:41:00 +0300

    first commit

---
+++ src/components/Products/Products.js
@@ -0,0 +1,21 @@
+import './Products.css';
+import Product from '../Product/Product'
+
+function Products (){
+    return(      
+    <section className="products">
+        
+ <Product/>
+ <Product/>
+ <Product/>
+ <Product/>
+ <Product/>
+ <Product/>
+ <Product/>
+ <Product/>
+ <Product/>
+ <Product/>
+</section>
+
+); }
+export default Products;
