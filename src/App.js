a25de15c663c8709140e39b076eccdb40afe63f1
yelisha <62861833+yelisha@users.noreply.github.com>
2021-10-10 18:57:01 +0300

    first good commit

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import Header from './components/Header/Header'
 function App() {
   return (
     <div className="App">
-     Header
-     Products
+    <Header/>
+    <Products/>
+
     </div>
   );
 }
