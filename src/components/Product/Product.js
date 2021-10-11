6fb63d6e61269a4bbb0f99b9065235d2ae0c14f2
yelisha <62861833+yelisha@users.noreply.github.com>
2021-10-11 20:24:12 +0300

    Use loop & props to render the products

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,38 +1,17 @@
 import "./Product.css";
 
-// function Product({
-//   id,
-//   title,
-//   price,
-//   description,
-//   category,
-//   image,
-//   rating: { rate, count },
-// }) {
-//   return (
-//     <div className="product">
-//       <div className="product-image">
-//         <img src={image} />
-//       </div>
-//       <div className="product-info">
-//         <h5>{title}</h5>
-//         <h6>{price}</h6>
-//       </div>
-//     </div>
-//   );
-// }
-
-function Product() {
+function Product({ id, title, price, description, category, image, rating }) {
   return (
     <div className="product-card">
       <div className="product-image">
-        <img src="https://cdn.shopify.com/s/files/1/0938/8938/products/10231100205_1_1315x1800_300_CMYK_1024x1024.jpeg?v=1445623369" />
+        <img src={image} />
       </div>
       <div className="product-info">
-        <h5>Winter Jacket</h5>
-        <h6>$99.99</h6>
+        <h5>{title}</h5>
+        <h6>{price}</h6>
       </div>
     </div>
   );
 }
+
 export default Product;
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,40 +1,23 @@
 import "./Products.css";
 import Product from "../Product/Product";
+import React from "react";
 
-// function Products({ products }) {
-//   return (
-//     <section className="products">
-//       {products.map((product) => (
-//         <h1 key={Product.id}>
-//           <Product
-//             id={product.id}
-//             title={product.title}
-//             price={product.price}
-//             description={product.description}
-//             category={product.description}
-//             image={product.image}
-//             rating={product.rating}
-//           />
-//         </h1>
-//       ))}
-//     </section>
-//   );
-
-function Products() {
+function Products({ productsList }) {
   return (
     <section className="products">
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
+      {productsList.map((product) => (
+        <Product
+          key={product.id}
+          id={product.id}
+          title={product.title}
+          price={product.price}
+          description={product.description}
+          category={product.category}
+          image={product.image}
+          rating={product.rating}
+        />
+      ))}
     </section>
   );
 }
-
 export default Products;
